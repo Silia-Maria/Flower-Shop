8b0395b5ce3c951c43546f1c946aded39e466a75
Silia Cronauer <silia.maria@web.de>
2023-03-23 13:56:00 +0100

    created components

--- flower-shop/src/app/app.module.ts
+++ flower-shop/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FlowersComponent } from './flowers/flowers.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FlowersComponent,
+    FooterComponent,
+    DetailsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
