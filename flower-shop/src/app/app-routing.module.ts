fa0ed05c1a1457a7fb02b78205f3cbfe4ffe8ad2
Silia Cronauer <silia.maria@web.de>
2023-03-23 16:21:22 +0100

    created navbar and started home + footer

--- flower-shop/src/app/app-routing.module.ts
+++ flower-shop/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { FlowersComponent } from './flowers/flowers.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: HomeComponent,
+}, {
+  path: "flowers", component: FlowersComponent,
+}, {
+  path: "details/:flowerId", component: DetailsComponent,
+}, {
+  path: "cart", component: ShoppingCartComponent,
+}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
