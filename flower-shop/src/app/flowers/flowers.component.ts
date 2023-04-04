4d8425aa1afef0c111ca309d974c13df8faf0e87
Silia Cronauer <silia.maria@web.de>
2023-04-04 13:04:03 +0200

    added filter method to flowers page

--- flower-shop/src/app/flowers/flowers.component.ts
+++ flower-shop/src/app/flowers/flowers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { flowers } from "../flowers";
 import { Idflowers } from '../Idflowers';
+import { CartService } from '../cart.service';
 
 
 @Component({
@@ -10,19 +11,42 @@ import { Idflowers } from '../Idflowers';
 })
 export class FlowersComponent implements OnInit {
 flowers: Idflowers [] = flowers;
+public flowersList: any;
+// public filterType: any;
+filterType = this.flowers;
 
-  constructor() { 
-  //    function filterFlowers() {
-  //     if (flowers.type == "Luxury flower") {
-        
-  //     }
-    
-  // };
+
+  constructor(private cartService: CartService) { 
+
+  }
+  addToCart (flower: Idflowers) {
+    window.alert("successfully added to Shopping Cart!");
+    this.cartService.addToCart(flower);
   }
 
   ngOnInit(): void {
+   
 
   }
- 
 
+// filter(type:string) {
+//   this.filterType = this.flowers
+//   .filter((a:any)=> {
+//     if(a.type == type || type == '') {
+//       return a;
+//     } 
+//   })
+// }
+
+filter (type: string) {
+  if (type === '') {
+    this.filterType = this.flowers;
+  } else {
+    this.filterType = this.flowers.filter((flower) => flower.type === type);
+  }
+}
+
+// filterFlowers(type: string) {
+//   return this.flowers.filter(flower => flower.type === type);
+// }
 }
