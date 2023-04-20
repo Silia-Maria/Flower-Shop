f9e764a98dfe076d1056f965d623fc18f489fb34
Silia Cronauer <silia.maria@web.de>
2023-04-20 15:34:00 +0200

    bestseller section media queries: click & hover

--- flower-shop/src/app/home/home.component.ts
+++ flower-shop/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd} from '@angular/router'; 
 import { flowers } from "../flowers";
 import { Idflowers } from '../Idflowers';
 
@@ -16,14 +17,34 @@ export class HomeComponent implements OnInit {
   // default image bestseller[0]
   displayedImage: string = '';
 
-  constructor() { }
+  // Bestseller click for smaller screens
+  isMobile: boolean = window.innerWidth < 769;
+
+  @HostListener('window:resize', ['$event']) 
+  onResize(event: any) {
+    this.isMobile = event.target.innerWidth < 769;
+  }
+
+  //Bestseller change image -> Small screens: click
+  onMobileClick (imageName: string) {
+    const matchingFlower = this.flowers.find(flower => flower.name === imageName && flower.bestseller === true);
+    if (matchingFlower !== undefined) {
+      if (this.displayedImage === matchingFlower.img) {
+        this.router.navigate(['/details', matchingFlower.id], {queryParams: {fromHome: true}});
+      } else {
+        this.displayedImage = matchingFlower.img;
+      }
+    } 
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.bestsellers = this.flowers.filter(flower => flower.bestseller === true);
     this.displayedImage = this.bestsellers[0].img;
   }
 
-  //Hover effect -> change image
+  //Bestseller change image -> big screens: hover
   onMouseEnter(imageName: string) {
     const matchingFlower = this.flowers.find(flower => flower.name === imageName && flower.bestseller === true);
     if(matchingFlower !== undefined) {
