507dcb67309fdfd04422746645557ea6a9b4cbbc
Silia Cronauer <silia.maria@web.de>
2023-04-24 13:10:06 +0200

    added paddingTop for navbar when toggler is active

--- flower-shop/src/app/navbar/navbar.component.ts
+++ flower-shop/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd} from '@angular/router'; 
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -7,17 +9,27 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  // @Output() filterType = new EventEmitter<string>();
-
-  // filterByType (type: string) {
-  // this.filterType.emit(type);
-  // }
-  
-
-  constructor() { }
+  isNavExpanded = false;
+  isHomePage = false;
+  private routerSub!: Subscription;
+ 
+  constructor(public route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+   this.routerSub = this.router.events.subscribe((event) => {
+    if(event instanceof NavigationEnd) {
+      this.isHomePage = this.router.url === '/';
+     
+    }
+   });
   }
 
+  ngOnDestroy(): void {
+    this.routerSub.unsubscribe();
+  }
+
+  onNavToggle(): void {
+    this.isNavExpanded = !this.isNavExpanded;
+  }
 
 }
